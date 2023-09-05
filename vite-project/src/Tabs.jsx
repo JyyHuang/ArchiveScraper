eef1d952209f9bd5a26333fcdca22520890447a2
Joey Huang <joeyh6547@gmail.com>
2023-09-05 12:32:49 -0400

    update

--- vite-project/src/Tabs.jsx
+++ vite-project/src/Tabs.jsx
@@ -4,8 +4,15 @@ export function Tabs() {
     return (
     <>
     <div className="tab-container">
-        <div className="ebay-container">eBay</div>
-        <div className="buyee-container">Mercari</div>
+        <div className="select">
+            <span>eBay</span>
+            <div className="dropdown-symbol"></div>       
+        </div>
+        <ul className="menu">
+            <li class="active">eBay</li>
+            <li>Buyee Auction</li>
+            <li>Mercari</li>        
+        </ul>    
     </div>
     </>
                 
