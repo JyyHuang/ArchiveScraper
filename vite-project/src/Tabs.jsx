7faa014f041f3c15842bca1121315111faa640e6
Joey Huang <joeyh6547@gmail.com>
2023-09-06 22:37:25 -0400

    added dropdown menu

--- vite-project/src/Tabs.jsx
+++ vite-project/src/Tabs.jsx
@@ -1,18 +1,31 @@
+import { useState } from "react"
 
 export function Tabs() {
+    
+    const [isActive, setIsActive] = useState(false)
+    const [selected, setSelected] = useState("eBay")
+    const [activeClass, setActiveClass] = useState(true)
 
+    const shops = ["eBay", "Buyee Auction", "Mercari"]
     return (
     <>
     <div className="tab-container">
-        <div className="select">
-            <span>eBay</span>
+        <div className="select" onClick={() => setIsActive(!isActive)}>      
+        <span>{selected}</span>
             <div className="dropdown-symbol"></div>       
         </div>
+        {isActive && (
         <ul className="menu">
-            <li class="active">eBay</li>
-            <li>Buyee Auction</li>
-            <li>Mercari</li>        
-        </ul>    
+        {shops.map((shop) => (
+            <li key={shop}
+                onClick={() => {
+                    setSelected(shop);
+                    setIsActive(false);
+                }
+            }>{shop}</li>
+        ))}             
+        </ul>
+        )}        
     </div>
     </>
                 
