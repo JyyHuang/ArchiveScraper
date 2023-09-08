a772337cff09e2300f7a38bccc124941860c2f18
Joey Huang <joeyh6547@gmail.com>
2023-09-08 15:20:38 -0400

    finished dropdown

--- vite-project/src/Home.jsx
+++ vite-project/src/Home.jsx
@@ -1,7 +1,5 @@
 export function Home(){
     return (
-        <div className="homepage">
-            <div className="homepage-text">Please Search...</div>
-        </div>
+        <div className="homepage">Please Search...</div>
     )
 }
